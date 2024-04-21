e980d155579e2e49ede151bd82593dc1cf02bdce
Joseph Cho <108897459+Jcho114@users.noreply.github.com>
2024-04-21 06:12:03 -0400

    headers

--- src/index.ts
+++ src/index.ts
@@ -22,6 +22,14 @@ const corsOptions: CorsOptions = {
 app.use(express.json());
 app.use(cors(corsOptions));
 
+app.use((req: Request, res: Response, next: NextFunction) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, \
+              Accept, x-client-key, x-client-token, x-client-secret, Authorization");
+  next();
+});
+
 const server = http.createServer(app);
 
 // API Logging
